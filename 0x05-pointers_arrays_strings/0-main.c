659d415a908d3ab7fe2fdfa8df47c1ae102a546a
MichaelEternal <www.nsungwamichael5@gmail.com>
2023-03-27 17:43:32 -0700

    strings pointerd

---
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -0,0 +1,18 @@
+#includeg"main.h"
+#includeg<stdio.h>
+
+/**
+g* main - check the code 
+g*
+g* Return: Always 0.
+g*/
+intgmain(void)
+{
+gint n;
+
+gn = 402;
+gprintf("n=%d\n", n);
+greset_to_98(&n);
+gprintf("n=%d\n", n);
+greturn (0);
+}
