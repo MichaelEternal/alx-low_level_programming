6bf79ccfd41578d08dada65c13198eed6e11993f
MichaelEternal <www.nsungwamichael5@gmail.com>
2023-03-30 20:41:27 -0700

    more poimters

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+intmain(void)
+{
+intn;
+inta[5];
+int*p;
+
+a[2] = 1024;
+p = &n;
+/*
+*writeyourlineofcodehere...
+*Remember:
+*-youarenotallowedtousea
+*-youarenotallowedtomodifyp
+*-onlyonestatement
+*-youarenotallowedtocodeanythingelsethanthislineofcode
+*/
+*(p + 5) = 98;
+/*...sothatthisprints98\n*/
+printf("a[2] = %d\n" , a[2]);
+return(0);
+}
