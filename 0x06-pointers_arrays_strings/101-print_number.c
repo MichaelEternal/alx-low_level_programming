77f7d3e994750a54e79bd088c954c2ee37c2f99c
MichaelEternal <www.nsungwamichael5@gmail.com>
2023-03-31 08:33:14 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,26 @@
 #include"main.h"
 
-/**
-*print_number-printnumberschars
-*@n:integerparams
-*Return:0
-*/
+/ * *
+* print_number-printnumberschars
+* @n : integerparams
+* Return : 0
+ */
 
 voidprint_number(intn)
 {
 unsignedintn1;
 
-n1=n;
+n1 = n;
 
-if(n<0)
+if (n < 0)
 {
-_putchar('-');
-n1=-n;
+_putchar (' -');
+n1 = -n;
 }
 
-if(n1/10!=0)
+if (n1 / 10 != 0)
 {
-print_number(n1/10);
+print_number(n1 / 10);
 }
-_putchar((n1%10)+'0');
+_putchar ((n1 % 10) + '0');
 }
