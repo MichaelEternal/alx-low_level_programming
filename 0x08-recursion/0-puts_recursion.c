c957c28bd6a3db1c89a9219c6c41b42f39753e14
MichaelEternal <www.nsungwamichael5@gmail.com>
2023-04-05 05:22:16 -0700

    1-main.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,12 @@
 */
 void_puts_recursion(char *s)
 {
-if (*s)
-{
-_putchar(*s);
-_puts_recursion(s + 1);
-}
-else
-_putchar('\n');
+   if (*s)
+   {
+      _putchar(*s);
+      _puts_recursion(s + 1);
+   }
+
+   else
+      _putchar('\n');
 }
