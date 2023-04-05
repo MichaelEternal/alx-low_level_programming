6bcab63ddd1cdf1e9d45e93e81b84907992ec989
MichaelEternal <www.nsungwamichael5@gmail.com>
2023-04-05 05:01:38 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+*_puts_recursion-functionlikeputs();
+*@s:input
+*Return:Always0(Success)
+*/
+void_puts_recursion(char *s)
+{
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+_putchar('\n');
+}
